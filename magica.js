8dd9fb9d6f9eaefbb58138d7a89d376cd17e0273
Jefferson Nascimento <91686329+JesonNascimento@users.noreply.github.com>
2021-11-09 15:35:44 -0300

    new

---
+++ magica.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+
+    //variavel
+    $surface = $('.superficie');
+    $carro = $('.carro');
+    $img = $('.carro img');
+    let flag = true;
+    const cars = ['./Img_05.png','./Img_06.png']
+
+    //keypress event
+    $(document).on('keypress', function(e){
+        if(e.which ==13){
+        $($surface).toggleClass('moveRight');
+        $($carro).toggleClass('suspension');}
+    })
+     //lights
+    $(document).on('keypress', function(e){
+        if(e.which ==119){
+            if(flag){
+                flag= false;
+                $img.attr('src', cars[0]);
+                
+            }else{
+                flag=true;
+                $img.attr('src', cars[1]);
+            }
+        }
+    })
+
+});
